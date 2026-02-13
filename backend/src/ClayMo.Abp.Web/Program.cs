83a462973f524459a7ed1e122eef780ae7962a39
杨洋 <1735671830@qq.com>
2026-02-13 09:33:30 +0800

    20

--- backend/src/ClayMo.Abp.Web/Program.cs
+++ backend/src/ClayMo.Abp.Web/Program.cs
@@ -5,6 +5,14 @@ public class Program
     public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Configuration.AddJsonFile(
+                "appsettings.Local.json",
+                optional: true,
+                reloadOnChange: true);
+        }
 
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
